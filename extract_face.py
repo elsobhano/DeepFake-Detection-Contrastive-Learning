96963501f1261e5dad3556b650fd10c958dd5f4d
elsobhano <sobhan23578@gmail.com>
2022-09-12 13:35:09 +0430

    Extracting facese of original sequences

--- extract_face.py
+++ extract_face.py
@@ -2,6 +2,7 @@ import cv2
 import os
 from src import detect_faces
 from PIL import Image
+import shutil
 import warnings
 warnings.filterwarnings("ignore")
 
@@ -45,9 +46,11 @@ def FrameCapture(path, dist_add):
 
 
 def extract_feature():
-    video_names = {}
+    shutil.rmtree('./Dataset/')
+    video_names_fake = {}
     count = 0
     fake_dataset = './faceforensic/manipulated_sequences'
+    real_dataset = './faceforensic/original_sequences/youtube'
     if os.path.exists(os.path.join('.','Dataset')) == False:
         os.mkdir(os.path.join('.','Dataset'))
     for dir1 in os.listdir(fake_dataset):
@@ -59,17 +62,36 @@ def extract_feature():
             for dir3 in os.listdir(os.path.join(fake_dataset, dir1, dir2)):
                 for filename in os.listdir(os.path.join(fake_dataset, dir1, dir2, dir3)):
                     video_add = os.path.join(fake_dataset, dir1, dir2, dir3, filename)
-                    if filename.split('.')[0] in video_names.keys():
-                        folder_name = video_names[filename.split('.')[0]]
+                    if filename.split('.')[0][:3] in video_names_fake.keys():
+                        folder_name = video_names_fake[filename.split('.')[0][:3]]
                     else:
                         folder_name = str(count)
-                        video_names[filename.split('.')[0]] = str(count)
+                        video_names_fake[filename.split('.')[0][:3]] = str(count)
                         count += 1
                     os.mkdir(os.path.join('./Dataset',dir2,dir1,folder_name))
                     dist_add = os.path.join('./Dataset',dir2, dir1, folder_name)
                     print('Start')
                     FrameCapture(video_add,dist_add)
                     print('Finish')
+                
+    print('*'*20)
+    print('Extracting for Original Videos')
+    for dir4 in os.listdir(real_dataset):
+        os.mkdir(os.path.join('.','Dataset',dir4,'Original'))
+        for dir5 in os.listdir(os.path.join(real_dataset,dir4)):
+            for vid_name in os.listdir(os.path.join(real_dataset,dir4,dir5)):
+                if vid_name.split('.')[0][:3] in video_names_fake.keys():
+                    folder_name = video_names_fake[filename.split('.')[0][:3]]
+                else:
+                    print('Error')
+                    print('{} is not in fake videos !!'.format(vid_name.split('.')[0][:3]))
+                os.mkdir(os.path.join('./Dataset',dir4,'Original',folder_name))
+                dist_add = os.path.join('./Dataset',dir4,'Original',folder_name)
+                vid_add = os.path.join(real_dataset,dir4,dir5,vid_name)
+                print('Start')
+                FrameCapture(vid_add,dist_add)
+                print('Finish')
+    print(video_names_fake)
                 
 extract_feature()
 print('Finish Extracting Frames')
